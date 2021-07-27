9e0aed240f7c42cc0225f9246ab7f35da43d7565
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-07-26 22:39:42 -0400

    push pre-updated files to new repo

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# DCS-PortfolioUpdate-No2
+# 08 Portfolio Assignment
+
+## Updating My Professional Materials
+
+This is the eighth assigment for MSU Coding Bootcamp; to update the portfolio we created ealier to include projects we have completed during our cohort. It also includes other updated professional materials, such as LinkedIn and GitHub profiles, and an updated resume.
+
+​
+
+### Live Links
+- [Daniel Shoup's Live Portfolio Page](https://danshoup.github.io/portfolio_DCS_2/)
+- [Daniel Shoup's GitHub Profile](https://github.com/danshoup)
+- [Daniel Shoup's LinkedIn Profile](https://www.linkedin.com/in/daniel-shoup-21873b2a/)
+- [Daniel Shoup's Online Resume](./assets/doc/resume2021.pdf)
+
+
+​
+
+## Requirements
+
+- Updataed portfolio feauring Project 1 and two exemplary homework assignments.
+- nav links scroll to appropriate section
+
+- Updated GitHub profile with pinned repositories featuring Project 1 and two exemplary assignments.
+
+- Updated resume.
+
+- Updated LinkedIn profile.
+
+
+### Updated Portfolio
+
+- Should display contact information.
+
+- Should have links to GitHub and LinkedIn profiles.
+
+- Should have a linke to downloadable PDF of resume.
+
+- Should display at least 2 examples of work from deployed projects or homework. Each should include:
+    - Project title
+    - Link to deployed version
+    - Link to the GitHub repository
+    - Screenshot of deployed application
+
+    #### Portfolio Design
+
+    - Should have a polished and professional look.
+    - Mobile-first design
+    - Color palette chosen to look other than default bootstrap theme or unstyled HTML.
+    - Reasonable readable font size and colors that don't strain the eyes.
+
+
+### Updated GitHub Profile
+
+- A profile picture other than the default.
+
+- Biography
+
+- Email address. (Personal or developer email.)
+
+- Link to deployed portfolio.
+
+- Pinned repositories.
+
+- Follow other developers.
+
+- Informative ReadMe files for all pinned projects.
+
+- Commit history for all pinned repositories. 
+
+### Updated Resume
+
+- A clear, concise, and compelling resume.
+
+- To include: 
+    - Professional heading.
+    - Technical skills section.
+    - 3 strong projects/assignments with brief description, languages used, and URL.
+
+### Updated LinkedIn
+
+- Strong bio statement, professional photo, and links to GitHub and portfolio.
+
---
+++ assets/images/Coding Quiz_files/script.js
@@ -0,0 +1,137 @@
+// Coding Quiz Assignment: JS Code
+
+// Create the questions array with question, choices, and the correct answer
+var questionData = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choice: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts"
+  },
+  {
+    question: "The condition in an 'if/else' statement is enclosed within ____________.",
+    choice: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    answer: "3. parentheses"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____________.",
+    choice: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4.  all of the above"
+  },
+  {
+    question: "String vavlues must be enclosed within _______________ when being assigned to variables.",
+    choice: ["1. commas", "2. curly braackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choice: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
+    answer: "4. console log"
+  },
+];
+
+// Variables
+
+var reStartButton = document.querySelector("#reStart");
+var clearHighScores = document.querySelector("#clearScores");
+var listButton = document.querySelectorAll(".lstBtn");
+var listItemTxt = document.querySelectorAll(".lstItem");
+
+
+var timerElement = document.querySelector("#timer-count");
+var startButton = document.querySelector("#start-button");
+var quizBox = document.querySelector(".questionBox");
+var quizQuestions = document.querySelector("#questions");
+var multipleChoice = document.querySelector("#choiceList");
+var listItems = document.querySelector(".items");
+var mult1 = document.querySelector("#choice1");
+var mult2 = document.querySelector("#choice2");
+var mult3 = document.querySelector("#choice3");
+var mult4 = document.querySelector("#choice4");
+var ansConfirm = document.querySelector(".answerConfirm");
+var timeLeft = 76;
+var score = 0;
+var questionIndex = 0;
+var currentQuestion = questionData[questionIndex].question;
+var currentAnswer = questionData[questionIndex].answer
+var choiceData = questionData[questionIndex].choice;
+
+
+// On page load init runs
+function init() {
+  multipleChoice.style.display = "none";
+}
+
+// Adds event listener to start quiz and timer on button click
+startButton.addEventListener("click", startQuiz);
+
+// Timer function set, and stops running when reaching 0;
+function startQuiz() {
+  startButton.style.display = "none";
+  multipleChoice.style.display = "block";
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    timerElement.textContent = timeLeft + " seconds";
+    
+       if (timeLeft <= 0) {
+      clearInterval(timeInterval);
+      timerElement.textContent = "TIME'S UP!";
+    }
+  }, 1000);
+
+  // Quiz questions start displaying when start button is pushed
+  getQuestion();
+
+};
+
+
+function getQuestion() {
+
+
+    // Current question is the index of the question that will be displayed from the question array
+  // Changes text content in the quizBox to current index question
+  quizQuestions.textContent = currentQuestion;
+  
+  // Changes each list item to one of the possible answers for question
+  mult1.textContent = choiceData[0];
+  mult2.textContent = choiceData[1];
+  mult3.textContent = choiceData[2];
+  mult4.textContent = choiceData[3];
+
+  // // for (i = 0; i < currentQuestion.length; i++) {
+    
+  // }
+ 
+    
+}
+
+
+  for (i of listButton) {
+    i.addEventListener("click", function(){
+      if (listItemTxt.textContent === currentAnswer) {
+        console.log("CORRECT");
+        ansConfirm.textContent = "That is CORRECT!"
+        score+10;
+        questionIndex++;
+        console.log(questionIndex)
+        getQuestion()
+      } else {
+        timeLeft-10;
+        questionIndex++;
+        ansConfirm.textContent = "Sorry, that is the WRONG asnwer...";
+        console.log("WRONG!");
+        getQuestion()
+      }
+
+    });
+  }
+
+
+  
+
+
+
+
+
+
+init();
+ 
